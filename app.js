f8b073c85e2687b2f1df02b475e1bdcfef215f61
Shaleen Badola <yourgamecoder@gmail.com>
2022-08-01 19:00:56 +0530

    fixing backend

--- app.js
+++ app.js
@@ -5,52 +5,30 @@ const cookieParser = require('cookie-parser');
 const tempRouter = require('./Routers/tempRouter');
 const cors = require('cors');
 const client_home = require('./secrets/secret').client_home
+const path = require('path');
 
 app.use(cors({
     origin:`${client_home}`,
     credentials:true,
 }));
 
-const port = process.env.PORT || 3001;
-
-app.use(express.static('public/build'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}))
-
-
 app.use('/user',userRouter);
 app.use('/temp',tempRouter);
 
-app.use(express.static('public'));
+const port = process.env.PORT || 3001;
+const isProduction = process.env.NODE_ENV === "production";
+console.log(process.env);
+isProduction && app.use(express.static(path.join(__dirname,"public","build")));
+isProduction &&
+  app.get("*", function (request, response) {
+    response.sendFile(path.join(__dirname,"public","build","index.html"));
+  });
+
+
 app.listen(port);
-
-app.get('/login',(req,res)=>{
-    res.json({
-        message:"hello world!"
-    })
-})
-
-app.get('/signup',(req,res)=>{
-    res.json({
-        message:"hello world!"
-    })
-})
-app.get('/',(req,res)=>{
-    res.json({
-        message:"hello world!"
-    })
-})
-app.get('/addsubject',(req,res)=>{
-    res.json({
-        message:"hello world!"
-    })
-})
-app.get('/subject/:subject_id',(req,res)=>{
-    res.json({
-        message:"hello world!"
-    })
-})
 app.get('/',(req,res)=>{
     res.json({
         message:"hello world!"
