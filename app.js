baced8de266837677d1eddd29df5244143e3e7e9
Shaleen Badola <yourgamecoder@gmail.com>
2022-08-01 17:24:04 +0530

    fixing bugs

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/user',userRouter);
 app.use('/temp',tempRouter);
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.listen(port);
 
 app.get('/',(req,res)=>{
