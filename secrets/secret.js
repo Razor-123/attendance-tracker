8c448edfc8a4694db304bd443bb0380d47fcd49b
Shaleen Badola <yourgamecoder@gmail.com>
2022-08-02 19:27:47 +0530

    Remove security files

--- secrets/secret.js
+++ secrets/secret.js
@@ -1,8 +1,4 @@
-module.exports = {
-    client_home:"https://attend-op.herokuapp.com",
-    JWT_KEY:"sdf234j2bjkb234",
-    db_link:"mongodb+srv://shaleen:sallu123@cluster0.sq3e6zs.mongodb.net/?retryWrites=true&w=majority"
-}
+
 
 // http://localhost:3000
 // https://attend-op.herokuapp.com
