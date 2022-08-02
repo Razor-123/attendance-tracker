843030ecd68f390a52f95ed3b57e390b4f3ee12e
Shaleen Badola <yourgamecoder@gmail.com>
2022-08-02 17:54:24 +0530

    Removed unwanted files

--- secrets/secret.js
+++ secrets/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
-    client_home:"http://localhost:3000",
+    client_home:"https://attend-op.herokuapp.com",
     JWT_KEY:"sdf234j2bjkb234",
     db_link:"mongodb+srv://shaleen:sallu123@cluster0.sq3e6zs.mongodb.net/?retryWrites=true&w=majority"
 }
